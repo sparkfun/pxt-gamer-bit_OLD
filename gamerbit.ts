ee5d50cb699617b043139bc4d3aedb727029bd9c
Peli de Halleux <jhalleux@microsoft.com>
2017-06-07 14:08:03 -0700

    Api cleanup

--- gamerbit.ts
+++ gamerbit.ts
@@ -3,21 +3,21 @@
  */
 //%
 enum GamerBitPin {
-	//% block="P0 (D-PAD up)"
+    //% block="P0 (D-PAD up)"
     P0 = <number>DAL.MICROBIT_ID_IO_P0,
-	//% block="P1 (D-PAD left)"
+    //% block="P1 (D-PAD left)"
     P1 = DAL.MICROBIT_ID_IO_P1,
-	//% block="P2 (D-PAD right)"
+    //% block="P2 (D-PAD right)"
     P2 = DAL.MICROBIT_ID_IO_P2,
-	//% block="P8 (D-PAD down)"
+    //% block="P8 (D-PAD down)"
     P8 = DAL.MICROBIT_ID_IO_P8,
-	//% block="P12 (Y button)"
+    //% block="P12 (Y button)"
     P12 = DAL.MICROBIT_ID_IO_P12,
-	//% block="P16 (X button)"
+    //% block="P16 (X button)"
     P16 = DAL.MICROBIT_ID_IO_P16,
-	//% block="P5 (A button)"
+    //% block="P5 (A button)"
     P5 = DAL.MICROBIT_ID_IO_P5,
-	//% block="P11 (B button)"
+    //% block="P11 (B button)"
     P11 = DAL.MICROBIT_ID_IO_P11,
 }
 
@@ -26,12 +26,12 @@ enum GamerBitPin {
  */
 //%
 enum GamerBitEvent {
-	//% block="down"
-	Down = DAL.MICROBIT_BUTTON_EVT_DOWN,
-	//% block="up"
-	Up = DAL.MICROBIT_BUTTON_EVT_UP,
-	//% block="click"
-	Click = DAL.MICROBIT_BUTTON_EVT_CLICK,
+    //% block="down"
+    Down = DAL.MICROBIT_BUTTON_EVT_DOWN,
+    //% block="up"
+    Up = DAL.MICROBIT_BUTTON_EVT_UP,
+    //% block="click"
+    Click = DAL.MICROBIT_BUTTON_EVT_CLICK,
 }
 
 /**
@@ -39,62 +39,36 @@ enum GamerBitEvent {
  */
 //% color=#f44242 icon="\uf11b"
 namespace gamerbit {
-	declare var pullsInitialized: number;
 	/**
 	 * 
 	 */
-	//% shim=gamerbit::init
-	function init(): void {
-		return;
-	}
-	
+    //% shim=gamerbit::init
+    function init(): void {
+        return;
+    }
+
 	/**
-	 * 
-	 */
-	//%
-	function configurePulls(): void {
-        if (pullsInitialized == 1) return;
-		pins.setPull(DigitalPin.P0, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P2, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P8, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P12, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P5, PinPullMode.PullUp);
-		pins.setPull(DigitalPin.P11, PinPullMode.PullUp);
-		pullsInitialized = 1;
-		return;
-	}
-	
-	/**
-	 * 
+	 * Determines if a button is pressed
 	 * @param button the pin that acts as a button
 	 */
-	//% weight=89
-	//% blockId=gamerbit_ispressed block="gamer:bit %button|is pressed"
-	//% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
-	export function isPressed(button: GamerBitPin): boolean {
-		configurePulls();
-		if(pins.digitalReadPin(<DigitalPin><number>button) == 0)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-//		return pins.digitalReadPin(<DigitalPin><number>button) != 0; // enable touch mode
-	}
+    //% weight=89
+    //% blockId=gamerbit_ispressed block="gamer:bit %button|is pressed"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    export function isPressed(button: GamerBitPin): boolean {
+        const pin = <DigitalPin><number>button;
+        pins.setPull(pin, PinPullMode.PullUp);
+        return pins.digitalReadPin(<DigitalPin><number>button) == 0;
+    }
 
 	/**
 	 * Registers code to run when a gamer:bit event is detected.
 	 */
-	//% weight=90
-	//% blockId=gamerbit_onevent block="gamer:bit on %button|%event"
-	//% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
-	//% event.fieldEditor="gridpicker" event.fieldOptions.columns=3
-	export function onEvent(button: GamerBitPin, event: GamerBitEvent, handler: Action) {
-		init();
-		control.onEvent(<number>button, <number>event, handler); // register handler
-	}
+    //% weight=90
+    //% blockId=gamerbit_onevent block="gamer:bit on %button|%event"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    //% event.fieldEditor="gridpicker" event.fieldOptions.columns=3
+    export function onEvent(button: GamerBitPin, event: GamerBitEvent, handler: Action) {
+        init();
+        control.onEvent(<number>button, <number>event, handler); // register handler
+    }
 }
